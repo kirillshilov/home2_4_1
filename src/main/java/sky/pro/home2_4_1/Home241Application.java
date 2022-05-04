a99d7058369546f309a6125d4e93a0617003ac2d
kirillshilov <sarataza@inbox.ru>
2022-05-04 20:53:16 +0300

    init

---
+++ src/main/java/sky/pro/home2_4_1/Home241Application.java
@@ -0,0 +1,13 @@
+package sky.pro.home2_4_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Home241Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Home241Application.class, args);
+    }
+
+}
---
+++ src/test/java/sky/pro/home2_4_1/Home241ApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.home2_4_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Home241ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
